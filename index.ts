70e0e67223b8961a9e74ff5c8b1744cf17a2b839
Mattias Holmlund <mattias@holmlund.se>
2022-08-26 16:04:53 +0200

    Cache expires old values

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const empty: unique symbol = Symbol('empty')
 
-export function cache<T>(fn: () => Promise<T>, _timeoutMs: number) {
+export function cache<T>(fn: () => Promise<T>, timeoutMs: number) {
     let cached: T | typeof empty = empty
+    let cached_updated = 0
     return async () => {
-        if (cached === empty) {
+        if (cached === empty || Date.now() - cached_updated > timeoutMs) {
+            cached_updated = Date.now()
             cached = await fn()
         }
         return cached
--- test/test-basic.ts
+++ test/test-basic.ts
@@ -16,6 +16,10 @@ test('calls original function', async (t) => {
 })
 
 let counter = 0
+function init_expensive_count() {
+    counter = 0
+}
+
 async function expensive_count() {
     await sleep(100)
     return counter++
@@ -23,6 +27,16 @@ async function expensive_count() {
 
 test('calls original function once', async (t) => {
     const cached = cache(expensive_count, 1000)
+    init_expensive_count()
     t.equal(await cached(), 0)
     t.equal(await cached(), 0)
+})
+
+test('calls original function again after timeout', async (t) => {
+    const cached = cache(expensive_count, 500)
+    init_expensive_count()
+    t.equal(await cached(), 0)
+    t.equal(await cached(), 0)
+    await sleep(1000)
+    t.equal(await cached(), 1)
 })
