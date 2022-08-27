67b624c1be5a379ea379e070aa955ab10043ea00
Mattias Holmlund <mattias@holmlund.se>
2022-08-27 10:00:26 +0200

    reject handling

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
-export function cache<T>(fn: () => Promise<T>, timeoutMs: number) {
-    let p_updated = 0
+export function cache<T>(
+    fn: () => Promise<T>,
+    timeoutMs: (success: boolean) => number
+) {
     let p: Promise<T> | undefined
+    let deadline = 0
     return async () => {
-        if (!p || Date.now() - p_updated > timeoutMs) {
-            p_updated = Date.now()
+        if (!p || Date.now() > deadline) {
+            deadline = Infinity
             p = fn()
+            p.then(() => {
+                deadline = Date.now() + timeoutMs(true)
+            }).catch(() => {
+                deadline = Date.now() + timeoutMs(false)
+            })
         }
         return await p
     }
--- test/test-basic.ts
+++ test/test-basic.ts
@@ -16,14 +16,14 @@ async function expensive_count() {
 }
 
 test('calls original function once', async (t) => {
-    const cached = cache(expensive_count, 1000)
+    const cached = cache(expensive_count, () => 1000)
     init_expensive_count()
     t.equal(await cached(), 0)
     t.equal(await cached(), 0)
 })
 
 test('calls original function again after timeout', async (t) => {
-    const cached = cache(expensive_count, 500)
+    const cached = cache(expensive_count, () => 500)
     init_expensive_count()
     t.equal(await cached(), 0)
     t.equal(await cached(), 0)
@@ -32,9 +32,82 @@ test('calls original function again after timeout', async (t) => {
 })
 
 test('calls original function once for concurrent requests', async (t) => {
-    const cached = cache(expensive_count, 500)
+    const cached = cache(expensive_count, () => 500)
     init_expensive_count()
     t.deepEqual(await Promise.all([cached(), cached(), cached()]), [0, 0, 0])
     await sleep(1000)
     t.deepEqual(await Promise.all([cached(), cached(), cached()]), [1, 1, 1])
+})
+
+test('caches rejects', async (t) => {
+    let calls = 0
+    const cached = cache(
+        () => Promise.reject(`reject ${calls++}`),
+        () => 500
+    )
+    init_expensive_count()
+    t.equal(await cached().catch((e) => `caught 1 ${e}`), 'caught 1 reject 0')
+    t.equal(await cached().catch((e) => `caught 2 ${e}`), 'caught 2 reject 0')
+    await sleep(1000)
+    t.equal(await cached().catch((e) => `caught 3 ${e}`), 'caught 3 reject 1')
+})
+
+test('does not clone responses', async (t) => {
+    const fn = async () => {
+        sleep(100)
+        return { a: 1 }
+    }
+
+    const cached = cache(fn, () => 500)
+
+    {
+        const res1 = await fn()
+        const res2 = await fn()
+        res1.a = 2
+        t.deepEqual(res2, { a: 1 })
+    }
+
+    {
+        const res1 = await cached()
+        const res2 = await cached()
+        res1.a = 2
+        t.deepEqual(res2, { a: 2 })
+    }
+})
+
+test('timeout starts after resolve', async (t) => {
+    let calls = 0
+    async function count() {
+        await sleep(500)
+        return calls++
+    }
+
+    const cached = cache(count, () => 300)
+    t.equal(await cached(), 0)
+    await sleep(200)
+    t.equal(await cached(), 0)
+    await sleep(1000)
+    t.equal(await cached(), 1)
+})
+
+test('different timeouts for resolve and reject', async (t) => {
+    let calls = 0
+    async function count() {
+        await sleep(500)
+        if (++calls % 2) {
+            return calls
+        } else {
+            throw `reject ${calls}`
+        }
+    }
+    const cached = cache(count, (success) => (success ? 500 : 100))
+    const tryCached = () => cached().catch((e) => `caught ${e}`)
+
+    t.equal(await tryCached(), 1)
+    await sleep(300)
+    t.equal(await tryCached(), 1)
+    await sleep(300)
+    t.equal(await tryCached(), 'caught reject 2')
+    await sleep(300)
+    t.equal(await tryCached(), 3)
 })
